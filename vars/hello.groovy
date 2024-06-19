c1c70506e75c26e67bc3ed804326e342083a4eef
Amit Kumar Nagar <nagar.amit1999@gmail.com>
2024-06-20 02:12:43 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,34 +1,5 @@
 // vars/echoGitUrl.groovy
 def call(String gitUrl) {
     echo "The provided Git URL is: ${gitUrl}"
-    pipeline {
-        agent any
-        stages {
-            stage('Checkout') {
-                steps {
-                    echo "Cloning repository from ${gitUrl}"
-                    // Git checkout step
-                    git url: "${gitUrl}"
-                }
-            }
-            stage('Build') {
-                steps {
-                    echo "Building the project..."
-                    // Add build steps here
-                }
-            }
-            stage('Test') {
-                steps {
-                    echo "Running tests..."
-                    // Add test steps here
-                }
-            }
-            stage('Deploy') {
-                steps {
-                    echo "Deploying the project..."
-                    // Add deployment steps here
-                }
-            }
-        }
-    }
+   
 }
